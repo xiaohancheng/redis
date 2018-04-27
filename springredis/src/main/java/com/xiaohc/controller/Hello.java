c35be525b49e72d7860123d1453332ca30556204
xiaohc <1242805755@qq.com>
2018-04-27 21:10:53 +0800

    spring redis集群环境搭建

--- springredis/src/main/java/com/xiaohc/controller/Hello.java
+++ springredis/src/main/java/com/xiaohc/controller/Hello.java
@@ -1,7 +1,11 @@
 package com.xiaohc.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
@@ -12,8 +16,16 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping("/hello")
 public class Hello {
+    @Autowired
+    private  RedisTemplate<String,String> redisTemplate;
     @RequestMapping("/sayHello")
     public String sayHello(){
        return "index";
+    }
+    @RequestMapping("/setAndGetHelloRedis/{value}")
+    @ResponseBody
+    public String setAndGetHelloRedis(@PathVariable("value") String value){
+        redisTemplate.opsForValue().set("helloRedis","hello "+value+",you set a value to redis!");
+        return redisTemplate.opsForValue().get("helloRedis");
     }
 }
